3717fc07d912fa873d80f305b0aaafd189d4221b
kauezao <kaue@kaue.com>
2011-09-29 19:38:39 -0300

    commit

--- ProvaJava28-09-2011/src/EstatisticasSimples.java
+++ ProvaJava28-09-2011/src/EstatisticasSimples.java
@@ -1,52 +1,50 @@
-
 public class EstatisticasSimples {
 
-public static double calculaValorMinimo(double[] valoresDeEntrada) {
-	double valorMinimoCalculado = valoresDeEntrada[0];
-	
-	for(int i=0; i<valoresDeEntrada.length; i++ ){
-		if(valoresDeEntrada[i] < valorMinimoCalculado){
-			valorMinimoCalculado = valoresDeEntrada[i];
+	public static double calculaValorMinimo(double[] valoresDeEntrada) {
+		double valorMinimoCalculado = valoresDeEntrada[0];
+
+		for (int i = 0; i < valoresDeEntrada.length; i++) {
+			if (valoresDeEntrada[i] < valorMinimoCalculado) {
+				valorMinimoCalculado = valoresDeEntrada[i];
+			}
 		}
+		return valorMinimoCalculado;
 	}
-return valorMinimoCalculado;
-}
 
-public static double calculaValorMaximo(double[] valoresDeEntrada) {
-	double valorMaximoCalculado = valoresDeEntrada[0];
-	for(int i=0; i<valoresDeEntrada.length; i++ ){
-		if(valoresDeEntrada[i] > valorMaximoCalculado){
-			valorMaximoCalculado = valoresDeEntrada[i];
+	public static double calculaValorMaximo(double[] valoresDeEntrada) {
+		double valorMaximoCalculado = valoresDeEntrada[0];
+		for (int i = 0; i < valoresDeEntrada.length; i++) {
+			if (valoresDeEntrada[i] > valorMaximoCalculado) {
+				valorMaximoCalculado = valoresDeEntrada[i];
+			}
 		}
+		return valorMaximoCalculado;
 	}
-return valorMaximoCalculado;
-}
 
-public static int calculaQuantidade(double[] valoresDeEntrada) {
-	int quantidadeDeValores = valoresDeEntrada.length; 
-return quantidadeDeValores;
-}
-
-public static double calculaMedia(double[] valoresDeEntrada) {
-	double somaDosValores = 0;
-	double mediaDeValores = 0;
-	for(int i=0; i<valoresDeEntrada.length; i++){
-		somaDosValores = somaDosValores + valoresDeEntrada[i];
+	public static int calculaQuantidade(double[] valoresDeEntrada) {
+		int quantidadeDeValores = valoresDeEntrada.length;
+		return quantidadeDeValores;
 	}
-	mediaDeValores = somaDosValores/valoresDeEntrada.length;
-return mediaDeValores;
-}
 
-public static double[] calcula(double[] valoresDeEntrada) {
-	double valoresDeSaida[] = {0,0,0,0};
-	
-	valoresDeSaida[0] =  calculaValorMinimo(valoresDeEntrada);
-	valoresDeSaida[1] =  calculaValorMaximo(valoresDeEntrada);
-	valoresDeSaida[2] =  calculaQuantidade(valoresDeEntrada);
-	valoresDeSaida[3] =  calculaMedia(valoresDeEntrada);
-	
-	return valoresDeSaida;
-	
+	public static double calculaMedia(double[] valoresDeEntrada) {
+		double somaDosValores = 0;
+		double mediaDeValores = 0;
+		for (int i = 0; i < valoresDeEntrada.length; i++) {
+			somaDosValores = somaDosValores + valoresDeEntrada[i];
+		}
+		mediaDeValores = somaDosValores / valoresDeEntrada.length;
+		return mediaDeValores;
+	}
 
-}
+	public static double[] calcula(double[] valoresDeEntrada) {
+		double valoresDeSaida[] = { 0, 0, 0, 0 };
+
+		valoresDeSaida[0] = calculaValorMinimo(valoresDeEntrada);
+		valoresDeSaida[1] = calculaValorMaximo(valoresDeEntrada);
+		valoresDeSaida[2] = calculaQuantidade(valoresDeEntrada);
+		valoresDeSaida[3] = calculaMedia(valoresDeEntrada);
+
+		return valoresDeSaida;
+
+	}
 }
